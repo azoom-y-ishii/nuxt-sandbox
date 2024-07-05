955f3726811db040b565c47e4235cd70e0a0c1de
Yoshinori Ishii <y.ishii@azoom.jp>
2024-07-05 20:27:36 +0900

    use-validation

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,68 @@
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <form>
+      <input
+        type="text"
+        v-model="user.name"
+        v-bind="attrs('name')"
+      >
+      <p>{{ isTouched('name') }}</p>
+      <p>{{ isDirty('name') }}</p>
+      <div>
+        <div
+          v-for="(link, index) in user.links"
+          :key="index"
+        >
+          <input
+            type="text"
+            v-model="user.links[index].name"
+            v-bind="attrs(['links', index, 'name'])"
+          > {{ isDirty(['links', index, 'name']) }}
+        </div>
+
+        <button
+          type="button"
+          @click="user.links.push({ name: '', url: '' })"
+        >
+          タグを追加
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { z } from 'zod'
+
+const schema = z.object({
+  name: z.string().max(10),
+  age: z.number(),
+  address: z.object({
+    country: z.string(),
+    city: z.string(),
+  }),
+  links: z.array(z.object({
+    name: z.string(),
+    url: z.string().url(),
+  })),
+})
+
+type User = z.infer<typeof schema>
+
+const user = ref<User>({
+  name: 'John Doe',
+  age: 30,
+  address: {
+    country: 'USA',
+    city: 'New York',
+  },
+  links: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com',
+    },
+  ],
+})
+
+const { isDirty, isValid, attrs, isTouched } = useValidation(user, schema)
+</script>
---
+++ test.ts
@@ -0,0 +1,3 @@
+const array = [1, 2, 3]
+
+console.log(array.at(-1))
