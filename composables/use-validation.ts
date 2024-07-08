844690a9bfd86260f6bb21ff1606433376919719
Yoshinori Ishii <y.ishii@azoom.jp>
2024-07-08 11:43:34 +0900

    Impl validation

---
+++ composables/use-validation.ts
@@ -0,0 +1,183 @@
+import type { ZodIssue, ZodSchema, z } from 'zod'
+import type { Paths, Split } from 'type-fest'
+import { triggerRef, watch } from 'vue'
+import { get, set } from 'lodash-es'
+
+// TODO It's just prototype. many refactorings are necessary
+type Check<A, B> = [A] extends [B] ? unknown : never
+type PathArray = (string | number)[]
+type ToStringArray<T extends (string | number)[]> = {
+  [P in keyof T]: `${T[P]}`;
+}
+type PathOrArray<TPathString, TPathStringArray, TPathArray extends PathArray> = TPathString | TPathArray & (TPathArray & Check<ToStringArray<TPathArray>, TPathStringArray>)
+
+type ValidationOptions = {
+  mode: 'lazy' // Currently only support lazy. 'aggressive' | 'passive' | 'lazy' | 'eager like vee validate
+}
+
+export const useValidation = <
+  const TSchema extends ZodSchema,
+  TPathString extends string = Exclude<Paths<z.infer<TSchema>>, never | number>,
+  TPathStringArray = Split<TPathString, '.'>,
+>(
+  valueRef: Ref,
+  schema: TSchema,
+  opts: ValidationOptions = { mode: 'lazy' },
+) => {
+  let issues: ZodIssue[] = []
+
+  const isInvalid = <const TPathArray extends PathArray>(pathOrArray?: PathOrArray<TPathString, TPathStringArray, TPathArray>) => {
+    const path = pathOrArray
+      ? (typeof pathOrArray === 'string' ? pathOrArray : pathOrArray.join('.'))
+      : undefined
+
+    // TOOD more simplify
+    if (opts.mode === 'lazy' && !isTouched(path as PathOrArray<TPathString, TPathStringArray, TPathArray>)) { return false }
+
+    if (!path) {
+      return Boolean(issues.length)
+    }
+
+    return issues.some((issue) => {
+      const issuePath = issue.path.join('.')
+      return issuePath === path || issuePath.startsWith(`${path}.`)
+    })
+  }
+
+  const isValid = <const TPathArray extends PathArray>(pathOrArray?: PathOrArray<TPathString, TPathStringArray, TPathArray>) => {
+    return !isInvalid(pathOrArray)
+  }
+
+  const listErrors = <const TPathArray extends PathArray>(pathOrArray: PathOrArray<TPathString, TPathStringArray, TPathArray>) => {
+    const path = typeof pathOrArray === 'string' ? pathOrArray : pathOrArray.join('.')
+
+    // TOOD more simplify
+    if (opts.mode === 'lazy' && !isTouched(path as PathOrArray<TPathString, TPathStringArray, TPathArray>)) { return [] }
+
+    return issues.filter((issue) => {
+      const issuePath = issue.path.join('.')
+      return issuePath === path || issuePath.startsWith(`${path}.`)
+    })?.map(x => x.message).flat()
+  }
+
+  const oneError = <const TPathArray extends PathArray>(...pathOrArrays: PathOrArray<TPathString, TPathStringArray, TPathArray>[]) => {
+    const errors = pathOrArrays.map(listErrors).flat()
+    return errors.length ? errors[0] : null
+  }
+
+  const stateMap = new WeakMap()
+
+  const checkState = <const TPathArray extends PathArray>(stateName: string, pathOrArray?: PathOrArray<TPathString, TPathStringArray, TPathArray>) => {
+    const path = pathOrArray
+      ? (typeof pathOrArray === 'string' ? pathOrArray.split('.') : pathOrArray as string[])
+      : undefined
+    const state = getState(path)
+    return get(state, [path?.at(-1) || '', stateName]) || false
+  }
+
+  const isDirty = <const TPathArray extends PathArray>(path?: Parameters<typeof checkState<TPathArray>>[1]) => checkState('dirty', path)
+
+  const isTouched = <const TPathArray extends PathArray>(path?: Parameters<typeof checkState<TPathArray>>[1]) => checkState('touched', path)
+
+  const getState = (path?: string[]) => {
+    let value = getProp(valueRef.value, path)
+    let state
+    if (typeof value === 'object') {
+      state = stateMap.get(value) ?? {}
+    } else {
+      if (!path) { throw new Error('path must be object') }
+      const parentPath = path.slice(0, -1)
+      value = parentPath.length ? get(valueRef.value, parentPath) : valueRef.value
+      state = stateMap.get(value) ?? {}
+    }
+    if (!Object.keys(state).length) { stateMap.set(value, state) }
+    return state
+  }
+
+  type AttrsOptions = {
+    primitive?: boolean
+  }
+
+  const attrs = <const TPathArray extends PathArray>(
+    pathOrArray: PathOrArray<TPathString, TPathStringArray, TPathArray>,
+    options: AttrsOptions = { primitive: false },
+  ) => {
+    const path = typeof pathOrArray === 'string' ? pathOrArray.split('.') : pathOrArray
+    const value = getProp(valueRef.value, path as string[])
+
+    const state = getState(path as string[])
+
+    const valueAttrName = options.primitive ? 'value' : 'modelValue'
+    const updateAttrName = options.primitive ? 'onInput' : 'onUpdate:modelValue'
+
+    return {
+      onBlur: () => {
+        set(state, [path.at(-1) || '', 'touched'], true)
+        triggerRef(valueRef)
+      },
+      [updateAttrName]: (eventOrData: any) => {
+        set(valueRef.value, path as string[], options.primitive ? eventOrData.target.value : eventOrData)
+        set(state, [path.at(-1) || '', 'dirty'], true)
+      },
+      invalid: isInvalid(pathOrArray),
+      [valueAttrName]: value,
+    }
+  }
+
+  const touch = <const TPathArray extends PathArray>(pathOrArray?: PathOrArray<TPathString, TPathStringArray, TPathArray>) => {
+    const path = pathOrArray
+      ? (typeof pathOrArray === 'string' ? pathOrArray.split('.') : pathOrArray) as string[]
+      : undefined
+    const value = getProp(valueRef.value, path)
+    traverseObjectDeep(value, (_, path) => {
+      const state = getState(path)
+      set(state, [path.at(-1) || '', 'touched'], true)
+    })
+
+    // eslint-disable-next-line no-console
+    console.log(issues)
+    triggerRef(valueRef)
+  }
+  watch(valueRef, (value) => {
+    const result = schema.safeParse(value)
+    if (result.success) {
+      issues = []
+    } else {
+      issues = result.error.issues
+    }
+  }, { deep: true, immediate: true })
+
+  return {
+    isValid,
+    isInvalid,
+    listErrors,
+    oneError,
+    attrs,
+    touch,
+    isTouched,
+    isDirty,
+  }
+}
+
+const getProp = (obj: any, path: string | string[] | undefined, defaultValue?: any) => {
+  if (!path || !path.length) { return obj }
+  return get(obj, path, defaultValue)
+}
+
+const traverseObjectDeep = (obj: any, cb: (value: any, path: string[], isObject: boolean) => void, path: string[] = []) => {
+  if (typeof obj === 'object') {
+    cb(obj, path, true)
+  } else {
+    cb(obj, path, false)
+    return
+  }
+  for (const key in obj) {
+    const value = obj[key]
+    if (typeof value === 'object') {
+      cb(value, [...path, key], true)
+      traverseObjectDeep(value, cb, [...path, key])
+    } else {
+      cb(value, [...path, key], false)
+    }
+  }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,132 @@
 <template>
-  <div>
-    <form>
+  <form
+    class="user-form"
+    novalidate
+  >
+    <div class="item">
+      <label class="label">Name:</label>
       <input
         type="text"
-        v-model="user.name"
-        v-bind="attrs('name')"
+        v-bind="attrs('name', { primitive: true })"
       >
-      <p>{{ isTouched('name') }}</p>
-      <p>{{ isDirty('name') }}</p>
-      <div>
-        <div
+    </div>
+    <div class="item">
+      <label class="label">Address:</label>
+      <input
+        type="text"
+        placeholder="County"
+        v-bind="attrs('address.country', { primitive: true })"
+      >
+      <input
+        type="text"
+        placeholder="City"
+        v-bind="attrs('address.city', { primitive: true })"
+      >
+    </div>
+    <div class="item">
+      <span class="label">Links:</span>
+      <ul class="links">
+        <li
           v-for="(link, index) in user.links"
           :key="index"
         >
           <input
             type="text"
-            v-model="user.links[index].name"
-            v-bind="attrs(['links', index, 'name'])"
-          > {{ isDirty(['links', index, 'name']) }}
-        </div>
+            placeholder="name"
+            v-bind="attrs(['links', index, 'name'], { primitive: true })"
+          >
+          <input
+            type="url"
+            placeholder="URL"
+            v-bind="attrs(['links', index, 'url'], { primitive: true })"
+          >
+        </li>
+      </ul>
+      <button
+        type="button"
+        @click="user.links.push({ name: '', url: '' })"
+      >
+        リンクを追加
+      </button>
+    </div>
+    <button
+      class="button -submit"
+      type="submit"
+      @click.prevent="onSubmit"
+    >
+      送信
+    </button>
+  </form>
 
-        <button
-          type="button"
-          @click="user.links.push({ name: '', url: '' })"
-        >
-          タグを追加
-        </button>
-      </div>
-    </form>
-  </div>
+  <table>
+    <thead>
+      <tr>
+        <th>Path</th>
+        <th>Valid</th>
+        <th>Touched</th>
+        <th>oneError</th>
+        <th>Errors</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>name</td>
+        <td>{{ isValid('name') }}</td>
+        <td>{{ isTouched('name') }}</td>
+        <td>{{ oneError('name') }}</td>
+        <td>{{ listErrors('name') }}</td>
+      </tr>
+      <tr>
+        <td>address.country</td>
+        <td>{{ isValid('address.country') }}</td>
+        <td>{{ isTouched('address.country') }}</td>
+        <td>{{ oneError('address.country') }}</td>
+        <td>{{ listErrors('address.country') }}</td>
+      </tr>
+      <tr>
+        <td>address.city</td>
+        <td>{{ isValid('address.city') }}</td>
+        <td>{{ isTouched('address.city') }}</td>
+        <td>{{ oneError('address.city') }}</td>
+        <td>{{ listErrors('address.city') }}</td>
+      </tr>
+      <tr
+        v-for="(link, index) in user.links"
+        :key="index"
+      >
+        <td>links {{ index }} name</td>
+        <td>{{ isValid(['links', index, 'name']) }}</td>
+        <td>{{ isTouched(['links', index, 'name']) }}</td>
+        <td>{{ oneError(['links', index, 'name']) }}</td>
+        <td>{{ listErrors(['links', index, 'name']) }}</td>
+      </tr>
+      <tr
+        v-for="(link, index) in user.links"
+        :key="index"
+      >
+        <td>links {{ index }} url</td>
+        <td>{{ isValid(['links', index, 'url']) }}</td>
+        <td>{{ isTouched(['links', index, 'url']) }}</td>
+        <td>{{ oneError(['links', index, 'url']) }}</td>
+        <td>{{ listErrors(['links', index, 'url']) }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script setup lang="ts">
 import { z } from 'zod'
 
 const schema = z.object({
-  name: z.string().max(10),
-  age: z.number(),
+  name: z.string().min(3).max(10),
   address: z.object({
-    country: z.string(),
-    city: z.string(),
+    country: z.string().min(1),
+    city: z.string().min(1),
   }),
   links: z.array(z.object({
-    name: z.string(),
-    url: z.string().url(),
+    name: z.string().min(1),
+    url: z.string().url().min(1),
   })),
 })
 
@@ -51,7 +134,6 @@ type User = z.infer<typeof schema>
 
 const user = ref<User>({
   name: 'John Doe',
-  age: 30,
   address: {
     country: 'USA',
     city: 'New York',
@@ -64,5 +146,50 @@ const user = ref<User>({
   ],
 })
 
-const { isDirty, isValid, attrs, isTouched } = useValidation(user, schema)
+const { attrs, isValid, isTouched, oneError, listErrors, touch } = useValidation(user, schema)
+
+const onSubmit = () => {
+  touch()
+  if (isValid()) {
+    // eslint-disable-next-line no-alert
+    alert('Valid!')
+  } else {
+    // eslint-disable-next-line no-alert
+    alert('Invalid!')
+  }
+}
 </script>
+
+<style lang="css" scoped>
+.user-form {
+  > .item > .label {
+    display: inline-block;
+    width: 100px;
+  }
+
+  > .item > .links {
+    margin: 5px;
+  }
+
+  > .button.-submit {
+    display: inline-block;
+    margin-top: 40px;
+  }
+}
+
+table {
+  width: 100%;
+  text-align: center;
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+table th {
+  padding: 10px;
+  background: #e9faf9;
+  border: solid 1px #778ca3;
+}
+table td {
+  padding: 10px;
+  border: solid 1px #778ca3;
+}
+</style>
